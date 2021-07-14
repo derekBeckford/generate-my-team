c86c7da668ac1b2b2ea5a1fc63cbc8670619c2be
Derek Beckford <dbeck1207@icloud.com>
2021-07-13 22:22:35 -0500

    starter employee logic and test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,9 @@
-const { expect } = require("@jest/globals");
-const Employee = require("../lib/Employee");
+const { expect } = require('@jest/globals');
+const Employee = require('../lib/Employee');
 
-test("creates a employee object", () => {
-  const employee = new Employee("Jan", 1, "jan@email.com");
+test('creates employee object', () => {
+    const employee = new Employee("jan", 1, "jan@email.com");
 
-  expect(employee.name).toBe("Jan");
-  expect(employee.id).toBe("1")
-  expect(employee.email).toBe("jan@email.com")
+    expect(employee.name).toBetoEqual(expect.any(String));
 });
+
